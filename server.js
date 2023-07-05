ebd52b259eeace4859bb236a0de61fd7f8ab0c38
Vivian Ferraro <55963058+viferraro@users.noreply.github.com>
2023-07-05 19:17:24 -0300

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('diva do devops');
+  res.send('kinga do devops');
 });
 
 app.get('*', (req, res) => {
